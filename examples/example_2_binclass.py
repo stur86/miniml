1e2f15236291ee7c2a15a9b4a2cc948a2897f9ee
Simone Sturniolo <simonesturniolo@gmail.com>
2025-08-25 15:46:14 +0200

    Expanded cross entropy loss to be multi-class and other loss functions to be parametrizable

--- examples/example_2_binclass.py
+++ examples/example_2_binclass.py
@@ -13,13 +13,13 @@ def _():
     import seaborn as sns
     import matplotlib.pyplot as plt
     from miniml import MiniMLModel, MiniMLParam
-    from miniml.loss import cross_entropy_log_loss, l2_regularization
+    from miniml.loss import CrossEntropyLogLoss, LNormRegularization
     return (
+        CrossEntropyLogLoss,
+        LNormRegularization,
         MiniMLModel,
         MiniMLParam,
-        cross_entropy_log_loss,
         jnp,
-        l2_regularization,
         mo,
         np,
         plt,
@@ -35,33 +35,28 @@ def _(jnp, np, plt, sns):
     X = jnp.array(rng.normal(size=(N,2)), dtype=jnp.float32)
     # Use a circular boundary for classification
     class_R = 1.2
-    y = (jnp.linalg.norm(X, axis=1) > class_R).astype(jnp.float32)
+    _is_out = jnp.linalg.norm(X, axis=1) > class_R
+    y = jnp.stack([~_is_out, _is_out], dtype=jnp.float32).T
 
     _fig, _ax = plt.subplots()
     _ax.set_aspect(1.0)
-    sns.scatterplot(x=X[:,0], y=X[:,1], c=y, ax=_ax)
+    sns.scatterplot(x=X[:,0], y=X[:,1], c=y[:,0], ax=_ax)
     _th = np.linspace(0, 2*np.pi, 100)
     _ax.plot(class_R*np.cos(_th), class_R*np.sin(_th), c='r')
     return N, X, class_R, y
 
 
 @app.cell
-def _(
-    MiniMLModel,
-    MiniMLParam,
-    cross_entropy_log_loss,
-    jnp,
-    l2_regularization,
-):
+def _(CrossEntropyLogLoss, LNormRegularization, MiniMLModel, MiniMLParam, jnp):
     class LogisticRegressor(MiniMLModel):
         def __init__(self):
-            self.M = MiniMLParam((1,6), reg_loss=l2_regularization)
-            super().__init__(cross_entropy_log_loss)
+            self.M = MiniMLParam((6,1), reg_loss=LNormRegularization(k=2, root=False))
+            super().__init__(CrossEntropyLogLoss(zero_ref=True))
 
         def predict(self, X):
             # Augment X with polynomial features
             X_aug = jnp.array([jnp.ones(len(X)), X[:,0], X[:,1], X[:,0]**2, X[:,0]*X[:,1], X[:,1]**2])
-            return (self.M.value@X_aug)[0]
+            return (X_aug.T@self.M.value)
     return (LogisticRegressor,)
 
 
@@ -72,7 +67,7 @@ def _(LogisticRegressor, X, y):
 
     logreg.fit(X, y, reg_lambda=1.0)
     y_hat_logits = logreg.predict(X)
-    y_hat = (y_hat_logits > 0.0)
+    y_hat = (y_hat_logits[:,0] > 0.0)
     return logreg, y_hat, y_hat_logits
 
 
@@ -107,15 +102,10 @@ def _(X, class_R, jnp, logreg, np, plt, sns, y_hat):
 def _(N, jnp, logreg, mo, y, y_hat, y_hat_logits):
     mo.md(
         f"""
-    Accuracy: {jnp.mean(y_hat==y):.2%}  
+    Accuracy: {jnp.mean(y_hat==y[:,0]):.2%}
     Average loss: {logreg.total_loss(y, y_hat_logits)/N:.2e}
     """
     )
-    return
-
-
-@app.cell
-def _():
     return
 
 
--- src/miniml/loss.py
+++ src/miniml/loss.py
@@ -1,12 +1,26 @@
-from jax.nn import softplus
+from abc import ABC, abstractmethod
+from jax.nn import log_softmax
 from jax import Array as JXArray
 import jax.numpy as jnp
-from functools import partial
 
 from typing import Callable
 
 RegLossFunction = Callable[[JXArray], JXArray]
 LossFunction = Callable[[JXArray, JXArray], JXArray]
+
+class LossFunctionBase(ABC):
+    
+    @abstractmethod
+    def __call__(self, y_true: JXArray, y_pred: JXArray) -> JXArray:
+        """Compute the loss between true and predicted values."""
+        pass
+    
+class RegLossFunctionBase(ABC):
+    
+    @abstractmethod
+    def __call__(self, y: JXArray) -> JXArray:
+        """Compute the regularization loss for the given values."""
+        pass
 
 def squared_error_loss(y_true: JXArray, y_pred: JXArray) -> JXArray:
     """Compute the squared loss between true and predicted values."""
@@ -22,17 +36,31 @@ def cross_entropy_loss(y_true: JXArray, y_pred: JXArray) -> JXArray:
     log_y_pred = jnp.log(jnp.clip(y_pred, 1e-15, 1 - 1e-15))
     return -jnp.sum(y_true * log_y_pred)
 
-def cross_entropy_log_loss(y_true: JXArray, log_y_pred: JXArray) -> JXArray:
-    """Compute the cross-entropy loss between true and predicted values,
-    assuming we're given the logits for the prediction."""
-    if y_true.shape != log_y_pred.shape:
-        raise ValueError("Shapes of true and predicted values must match.")
-    return -jnp.sum(y_true * log_y_pred)+jnp.sum(softplus(log_y_pred))
 
+class CrossEntropyLogLoss(LossFunctionBase):
+    
+    def __init__(self, zero_ref: bool = False) -> None:
+        self.zero_ref = zero_ref
+        
+    def __call__(self, y_true: JXArray, log_y_pred: JXArray) -> JXArray:
+        if self.zero_ref:
+            # Add a zero reference category
+            zero_shape = log_y_pred.shape[:-1] + (1,)
+            log_y_pred = jnp.concatenate([log_y_pred, jnp.zeros(zero_shape)], axis=-1)
 
-def norm_regularization(y: JXArray, p: float) -> JXArray:
-    """Compute the norm regularization term."""
-    return jnp.sum(jnp.abs(y) ** p)
+        if y_true.shape != log_y_pred.shape:
+            raise ValueError("Shapes of true and predicted values must match.")
 
-l2_regularization = partial(norm_regularization, p=2.0)
-l1_regularization = partial(norm_regularization, p=1.0)
+        log_y_pred = log_softmax(log_y_pred, axis=-1)
+        return -jnp.sum(y_true * log_y_pred)
+
+class LNormRegularization(RegLossFunctionBase):
+    def __init__(self, k: int = 2, root: bool = False) -> None:
+        self.k = k
+        self.return_root = root
+        
+    def __call__(self, y: JXArray) -> JXArray:
+        ans = jnp.sum(jnp.abs(y) ** self.k)
+        if self.return_root:
+            return ans**(1.0/self.k)
+        return ans
