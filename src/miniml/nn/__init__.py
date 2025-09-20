9831283f7faa5a11c150dda52f69a00188160dfa
Simone Sturniolo <simonesturniolo@gmail.com>
2025-09-20 10:15:05 +0100

    Added RBF related functions and classes to exports of nn

--- src/miniml/nn/__init__.py
+++ src/miniml/nn/__init__.py
@@ -1,4 +1,5 @@
 """Utilities for common ML model architectures"""
+
 from miniml.nn.activations import (
     relu,
     sigmoid,
@@ -9,9 +10,21 @@ from miniml.nn.activations import (
     gelu,
     silu,
 )
+from miniml.nn.rbf import (
+    RBFunction,
+    gaussian_rbf,
+    multiquadric_rbf,
+    inverse_quadratic_rbf,
+    inverse_multiquadric_rbf,
+    linear_rbf,
+    r_tanh_rbf,
+    bump_rbf,
+    PolyharmonicRBF,
+)
 from miniml.nn.linear import Linear
 from miniml.nn.stack import Stack
 from miniml.nn.mlp import MLP
+from miniml.nn.rbfnet import RBFLayer
 
 __all__ = [
     "Linear",
@@ -25,4 +38,14 @@ __all__ = [
     "gelu",
     "silu",
     "LeakyReLU",
+    "RBFunction",
+    "gaussian_rbf",
+    "multiquadric_rbf",
+    "inverse_quadratic_rbf",
+    "inverse_multiquadric_rbf",
+    "linear_rbf",
+    "r_tanh_rbf",
+    "bump_rbf",
+    "PolyharmonicRBF",
+    "RBFLayer",
 ]
